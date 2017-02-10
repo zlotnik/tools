d5b8ff9c7e8bf56a49541816ed94b1ebe4495aa9
Wojciech Marzec <wojomc81@gmail.com>
2017-02-10 18:05:27 +0100

    a draft of CategoryPage object with his child

---
+++ BookmakerOfferDownloader/FirstLevelPageCategory.pm
@@ -0,0 +1,20 @@
+package FirstLevelPageCategory;
+
+use CategoryPage;
+use warnings;
+use strict;
+
+our @ISA = qw(CategoryPage);
+
+sub getAllSubCategories($)
+{
+	return "I am the FirstLevelPageCategory\n";
+
+
+}
+
+
+1;
+
+
+
