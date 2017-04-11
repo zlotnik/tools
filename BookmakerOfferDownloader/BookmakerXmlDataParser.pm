fb913d4c7437548f1d57279a1693c26b946ba3cf
Wojciech Marzec <wojomc81@gmail.com>
2017-04-11 13:06:48 +0200

    [I] continuing work on implementation BookmakerXmlDataParser.pm: partial corectness

--- BookmakerOfferDownloader/BookmakerXmlDataParser.pm
+++ BookmakerOfferDownloader/BookmakerXmlDataParser.pm
@@ -10,13 +10,27 @@ sub new();
 sub isCorectBookmakerSelectorFile($);
 sub isCorectBookmakerOfferFile($);
 sub xmlSelectorContainsAllNeededData($);
-
+sub isCorrectBookmakerName($);
+sub isCorrectDisciplineName($);
 #################DEFINITION####################################
 sub new()
 {
 	my $class = shift;
 	my $self = bless {}, $class;
 	return $self;
+};
+
+sub isCorectBookmakerSelectorFile($)
+{
+	my $pathToXmlSelector = $_[0];
+	my $xmlParser = XML::LibXML->new; 
+	my $isCorrectXmlFile = $xmlParser->parse_file($pathToXmlSelector); 
+			
+	if ( $isCorrectXmlFile and xmlSelectorContainsAllNeededData($pathToXmlSelector) )
+	{
+		return 1;
+	}
+	return 0;
 };
 
 sub isCorectBookmakerOfferFile($)
@@ -28,22 +42,10 @@ sub isCorrectEventListFile($)
 	return 0;
 };
 
-sub isCorectBookmakerSelectorFile($)
-{
-	my $xmlParser = XML::LibXML->new; 
-	my $isCorrectXmlFile = $xmlParser->parse_file($pathToXmlSelector); 
-	my $pathToXmlSelector = $_[0];
-			
-	if ( $isCorrectXmlFile and xmlSelectorContainsAllNeededData($pathToXmlSelector) )
-	{
-		return 1;
-	}
-	return 0;
-};
-
 sub xmlSelectorContainsAllNeededData($)
 {
-	return (isOneOrMoreBookmakersSpecifiedInXmlSelector($) and isOneOrMoreDyscyplineSpecified($));
+	my $xmlSelector = $_[0];
+	return (isOneOrMoreBookmakersSpecifiedInXmlSelector($xmlSelector) and isOneOrMoreDyscyplineSpecified($xmlSelector));
 
 };
 
@@ -61,22 +63,64 @@ sub isOneOrMoreBookmakersSpecifiedInXmlSelector($)
 
 sub countHowManyBookmakerIsChoosedInXmlSelector($)
 {
+	my $choosenBookmakers = $doc->findnodes("/note/dataSources")->[0];
+	
+	
+	if (not defined($choosenBookmakers))
+	{
+		return 0 ;
+		#'Iam not sure if it is correct formula';
+	}
 
+	my $bookmakerCounter = 0;
+	foreach($choosenBookmakers->nonBlankChildNodes())
+	{
+		my $disciplineName = $_;
+		if(isCorrectBookmakerName($disciplineName))
+		{
+			$bookmakerCounter++;			
+		}
+		else
+		{
+			return 0; #instead of returning 0 should be parser validating all bookmakers and requirement that all bookmaker name must be 'legal'
+		}
+	}
+	return $bookmakerCounter++;
+	
 }
 
 
+sub isCorrectBookmakerName($)
+{
+	my $bookmakerNamer = $_;
+	if( 'betexplorer' eq $bookmakerNamer)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+sub isCorrectDisciplineName($)
+{
+	my $disciplineName = $_;
+	my @supportedDisciplines = ('soccer'); 
+	if ( grep( /^$disciplineName$/, $supportedDisciplines ) )
+	{
+		return 1;
+	}
+	return 0;
+}
+
 sub isOneOrMoreDyscyplineSpecified($)
 {
-	my $xmlDoc = $_[0];
-	
+	my $xmlDoc = $_[0];	
 	
 	
 	if(countHowManyDisciplinesToDownloadIsDefinedInXmlSelector() > 0)
 	{
 		return 1;
 	}
-	return 0;
-	
+	return 0;	
 		
 };
 
@@ -84,20 +128,27 @@ sub countHowManyDisciplinesToDownloadIsDefinedInXmlSelector()
 {
 	my $dataChoosenToDownloadXmlNode = $doc->findnodes("/note/dataChoosenToDownload")->[0];
 	
-	die 'Iam not sure if it is correct formula';
+	
 	if (not defined($dataChoosenToDownloadXmlNode))
 	{
 		return 0 ;
+		#'Iam not sure if it is correct formula';
 	}
 
+	my $bookmakerCounter = 0;
 	foreach($dataChoosenToDownloadXmlNode->nonBlankChildNodes())
 	{
 		my $disciplineName = $_;
-			if(not isCorrectDisciplineName($disciplineName))
-			{
-				return 0;
-			}
+		if(isCorrectDisciplineName($disciplineName))
+		{
+			$bookmakerCounter++;			
+		}
+		else
+		{
+			return 0; #instead of returning 0 should be parser validating all disciplnes and requirement that all bookmaker name must be 'legal'
+		}
 	}
+	return $bookmakerCounter++;
 }
 
 
