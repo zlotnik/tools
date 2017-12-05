56b8013073861dbdfe9b12ec6284aae857152187
Wojtek Marzec <wojomc81@gmail.com>
2017-12-05 17:19:37 +0100

    [T] removed redudant code from parsers's test

--- BookmakerOfferDownloader/tests/legacy/parsingDataTest.pl
+++ BookmakerOfferDownloader/tests/legacy/parsingDataTest.pl
@@ -1,14 +1,14 @@
 use strict;
 use warnings;
-use Test::More tests => 4;
+use Test::More tests => 5;
 use lib '..';
 use BookmakerXmlDataParser;
-use DataDownloader; #needed??
 use WojtekToolbox;
 
 use FindBin;
 use File::Copy;
 
+#TODO: parsers should be moved to separated directory outside this module
 
 print "****TEST SUITE PARSING DATA*****\n\n"; 
 my $correctBookmakerSelectorFile = "$FindBin::Bin/../../input/parameters/examples/ekstraklasaSelector.xml";
@@ -18,20 +18,18 @@ my $mockedRawDataPath = "$FindBin::Bin/../../tmp/rawDataMockFile";
 
 my $aBookmakerXmlDataParser = BookmakerXmlDataParser->new(); 
 
-(-e $mockedRawDataPath) or die "File doesn't exist $mockedRawDataPath\n";
 ok($aBookmakerXmlDataParser->isCorrectRawDataFile($mockedRawDataPath), 
 															  "Parsing raw data file\n $mockedRawDataPath\n" ) or die;
 
-(-e $correctBookmakerSelectorFile) or die "File doesn't exist $correctBookmakerSelectorFile\n";
 ok($aBookmakerXmlDataParser->isCorectBookmakerDataSelectorFile($correctBookmakerSelectorFile),
 															  "Parsing selector file\n $correctBookmakerSelectorFile\n") or die;
 
-(-e $correctDownloadedBookmakerOfferFile) or die "File doesn't exist $correctDownloadedBookmakerOfferFile\n";
 ok($aBookmakerXmlDataParser->isCorectDownloadedBookmakerOfferFile($correctDownloadedBookmakerOfferFile),
 															   "Parsing bookmaker downloaded offert file\n $correctDownloadedBookmakerOfferFile\n")or die;
 
-(-e $correctBookmakerEventList) or die "File doesn't exist $correctBookmakerEventList\n";
-
+ok($aBookmakerXmlDataParser->isCorrectRawDataFile($mockedRawDataPath), 
+															  "Parsing raw data file\n $mockedRawDataPath\n" ) or die;
+															   
 if(isConnectedToInternet())
 {
 	ok($aBookmakerXmlDataParser->isCorrectEventListFile($correctBookmakerEventList),"Parsing event list file\n $correctBookmakerEventList\n");
