790c3cee424812f9bc91c85626799d2987007832
Wojciech Marzec <wojomc81@gmail.com>
2017-12-13 16:26:15 +0100

    [T] 1.test renamed 2. Fix  for copying offer file from BookmakerDownloader

--- SurebetsFinder/tests/incremental/findingSurebetsTest.pl
+++ SurebetsFinder/tests/incremental/findingSurebetsTest.pl
@@ -13,22 +13,20 @@ print "****TEST MOCKED BETEXPLORER DOWNLOADER*****\n\n";
 
 #copy .. offer file
 #instead of creating advanced surebet parser maybe it is better to create model input and output data and compare their checksum in test(both input and output)
-
 #later create advanced surebet parser   
-
-
-copy "$FindBin::Bin/../../input/bookmakersOffers_generatedByMock.xml", 'input/bookmakersOffers_generatedByMock.xml' or die;
-my $pathToBookmakersOffeerFile = "$FindBin::Bin/../../input/bookmakersOffers_generatedByMock.xml";
+	
+copy "../BookmakerOfferDownloader/output/downloadedPolandEkstraklasa_mockednet.xml", 'input/bookmakersOffers_generatedByMock.xml' or die;
+my $pathToBookmakersOfferFile = "$FindBin::Bin/../../input/bookmakersOffers_generatedByMock.xml";
 my $surebetsOutputFile = "$FindBin::Bin/../../output/test/surebetsPolandEkstraklasa.xml";
 my $aBookmakerXmlDataParser = BookmakerXmlDataParser->new(); 
 
 my $theSurebetFinder = SurebetFinder->new();
 
-$theSurebetFinder->loadBookmakersOfferFile($pathToBookmakersOffeerFile);
+$theSurebetFinder->loadBookmakersOfferFile($pathToBookmakersOfferFile);
 
 my $amountOfSurebetsFound = $theSurebetFinder->generateSurebetsFile($surebetsOutputFile);
 my $isSurebtsFileGeneratedCorrectly = $aBookmakerXmlDataParser->isCorrectSurebetsFile($surebetsOutputFile);
-ok($isSurebtsFileGeneratedCorrectly, "SurebetFinder: checking syntax of generated surebets file") or die;
+ok($isSurebtsFileGeneratedCorrectly, "SurebetFinder: checking syntax of generated surebets file");
 ok($amountOfSurebetsFound == 3, "SurebetFinder: checking amount of generated surebets");
 
 #maybe test files should have _test postfix
