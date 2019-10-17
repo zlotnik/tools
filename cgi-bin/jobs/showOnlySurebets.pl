32cf76a7c44b9257acace57ed8ebb20419cd6700
Wojciech Marzec <wojomc81@gmail.com>
2019-10-17 12:59:45 +0200

    [I] script for choosing data for surebet table seems to have all needed fields

--- cgi-bin/jobs/showOnlySurebets.pl
+++ cgi-bin/jobs/showOnlySurebets.pl
@@ -12,10 +12,12 @@ sub fetch_EventName_fromEventBestCombinationNode($);
 sub fetch_1_Price_fromEventBestCombinationNode($);
 sub fetch_X_Price_fromEventBestCombinationNode($);
 sub fetch_2_Price_fromEventBestCombinationNode($);
-sub fetch_Bookmaker_1_fromEventBestCombinationNode($);
-sub fetch_Bookmaker_X_fromEventBestCombinationNode($);
-sub fetch_Bookmaker_2_fromEventBestCombinationNode($);
+sub fetch_BookmakerName_1_fromEventBestCombinationNode($);
+sub fetch_BookmakerName_X_fromEventBestCombinationNode($);
+sub fetch_BookmakerName_2_fromEventBestCombinationNode($);
 sub fetch_profit_fromEventBestCombinationNode($);
+sub fetch_Price_fromEventBestCombinationNode($$);
+sub fetch_BookmakerName_fromEventBestCombinationNode($$);
 
 defined $ARGV[0] or die "You must specify a path to directory with bookmaker offer files"; 
 
@@ -40,24 +42,19 @@ sub showMySureBets($)
 	
 
 		my $profit = fetch_profit_fromEventBestCombinationNode($surebet_node);
-		
-		
-		my $event_name = fetch_EventName_fromEventBestCombinationNode($surebet_node);
-		my $price_1 = fetch_1_Price_fromEventBestCombinationNode($surebet_node);
-		my $price_X = fetch_X_Price_fromEventBestCombinationNode($surebet_node);
-		my $price_2 = fetch_2_Price_fromEventBestCombinationNode($surebet_node);
-		my $bookmaker_1 = fetch_Bookmaker_1_fromEventBestCombinationNode($surebet_node);
-		my $bookmaker_X = fetch_Bookmaker_X_fromEventBestCombinationNode($surebet_node);
-		my $bookmaker_2 = fetch_Bookmaker_2_fromEventBestCombinationNode($surebet_node);
-		
 		if($profit != 1) #temporary workaround untill issue with empty nodes will be solved in backend functionality
-		{
+		{			
+			my $event_name = fetch_EventName_fromEventBestCombinationNode($surebet_node);
+			my $price_1 = fetch_1_Price_fromEventBestCombinationNode($surebet_node);
+			my $price_X = fetch_X_Price_fromEventBestCombinationNode($surebet_node);
+			my $price_2 = fetch_2_Price_fromEventBestCombinationNode($surebet_node);
+			my $bookmaker_1 = fetch_BookmakerName_1_fromEventBestCombinationNode($surebet_node);
+			my $bookmaker_X = fetch_BookmakerName_X_fromEventBestCombinationNode($surebet_node);
+			my $bookmaker_2 = fetch_BookmakerName_2_fromEventBestCombinationNode($surebet_node);			
 			$toReturn = "EventName: $event_name PROFIT: $profit ";
 			$toReturn .= "bookmaker_1 $bookmaker_1 bookmaker_x $bookmaker_X bookmaker_2 $bookmaker_2 ";
-			$toReturn .= "price_1 $price_1 price_X $price_X price_2 $price_2\n"
-		}
-
-		
+			$toReturn .= "price_1 $price_1 price_X $price_X price_2 $price_2\n";
+		}		
 	}
 
 	return $toReturn;
@@ -85,10 +82,6 @@ sub fetch_EventName_fromEventBestCombinationNode($)
 
 };
 
-sub fetch_Price_fromEventBestCombinationNode($$);
-sub fetch_Bookmaker_fromEventBestCombinationNode($$);
-
-sub fetch_Bookmaker_fromEventBestCombinationNode($$);
 
 sub fetch_Price_fromEventBestCombinationNode($$)
 {
@@ -120,22 +113,29 @@ sub fetch_2_Price_fromEventBestCombinationNode($)
 	
 };
 
-sub fetch_Bookmaker_1_fromEventBestCombinationNode($)
+sub fetch_BookmakerName_1_fromEventBestCombinationNode($)
 {
 	my ($surebet_node) = @_;
-	return 'TODO';
+	my $betOption = '1';
+
+	return fetch_BookmakerName_fromEventBestCombinationNode($surebet_node, $betOption);
+	
 };
 
-sub fetch_Bookmaker_X_fromEventBestCombinationNode($)
+sub fetch_BookmakerName_X_fromEventBestCombinationNode($)
 {
 	my ($surebet_node) = @_;
-	return 'TODO';
+	my $betOption = 'X';
+
+	return fetch_BookmakerName_fromEventBestCombinationNode($surebet_node, $betOption);
 };
 
-sub fetch_Bookmaker_2_fromEventBestCombinationNode($)
+sub fetch_BookmakerName_2_fromEventBestCombinationNode($)
 {
 	my ($surebet_node) = @_;
-	return 'TODO';
+	my $betOption = '2';
+
+	return fetch_BookmakerName_fromEventBestCombinationNode($surebet_node, $betOption);
 };
 
 sub showAllSurrebetsInDirectory($)
@@ -167,3 +167,11 @@ sub showAllSurrebetsInDirectory($)
 	return $allSurebets;
 }
 	 
+sub fetch_BookmakerName_fromEventBestCombinationNode($$)
+{
+	my ($surebet_node, $betOption) = @_;
+
+	my @offerNode = $surebet_node->findnodes("bestCombinations/_1X2/_${betOption}/*");	
+	$offerNode[0] =~ /\<_(\w+)\>/;	
+	return $1;	
+}
