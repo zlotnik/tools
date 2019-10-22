e0bee41ad3f4e531998b71e0b1bcc187d2c31344
Wojciech Marzec <wojomc81@gmail.com>
2019-10-22 18:37:37 +0200

    [I] added argument for defining random delay for fetching data in order to not generate constant intens traffic

--- cgi-bin/runForAll_inputFiles.pl
+++ cgi-bin/runForAll_inputFiles.pl
@@ -1,6 +1,22 @@
 #!/bin/perl
 
+sub randomSecond_Delay($$);
 my @selectorFiles = <input/*.xml>;
+
+my $delayInSeconds_from;
+my $delayInSeconds_to;
+if(defined $ARGV[0])
+{
+	if($ARGV[0] =~ /--delay=(\d+)\.\.(\d+)/ )
+	{
+		$delayInSeconds_from = $1;
+		$delayInSeconds_to = $2; 
+	}
+	else 
+	{
+		print "WARNING incorrect argument\n"
+	}
+}
 
 
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
@@ -9,7 +25,6 @@ my $timeStamp = $year +1900 . $mon+1 . $mday . '_' . $hour . '_' . $min . '_' .$
 my $outputDirectory = "results/${timeStamp}";
 
 mkdir $outputDirectory or die "Error during creating output directory $outputDirectory \nndToExecute"; 
-
 
 foreach(@selectorFiles)
 {
@@ -20,8 +35,19 @@ foreach(@selectorFiles)
 
 	my $outputFilePath = "${outputDirectory}/${outputFileName_prefix}_profitability.xml";
 
+	if(defined $delayInSeconds_from)
+	{
+		randomSecond_Delay($delayInSeconds_from, $delayInSeconds_to);
+	}
+	
 	my $commandToExecute = "./surebetCrafter.sh $selectorFileName $outputFilePath";  
-	(system($commandToExecute) == 0) or die $!;
-	#`$commandToExecute`;die $commandToExecute;
+	(system($commandToExecute) == 0) or die $!;	
 }
 
+
+sub randomSecond_Delay($$)
+{
+	my ($from, $to) = @_;
+	my $delay = int(rand($to)) + $from;
+	sleep($delay);
+}
