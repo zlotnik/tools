be8992cbf8cde16d4e359d053a4d388f3f870cb9
Wojciech Marzec <wojomc81@gmail.com>
2019-12-04 11:21:32 +0100

    [I] removed obsolete Windows scripts

--- cgi-bin/surebetCrafter_runAll.cmd
+++
@@ -1,7 +0,0 @@
-perl surebetCrafter.pl .\input\ekstraklasaSelector.xml .\results\ekstraklasa_Output.xml
-perl surebetCrafter.pl .\input\poland_3liga_group1_selector.xml .\results\poland_3liga_group1_Output.xml
-perl surebetCrafter.pl .\input\poland_3liga_group2_selector.xml .\results\poland_3liga_group2_Output.xml
-perl surebetCrafter.pl .\input\poland_3liga_group3_selector.xml .\results\poland_3liga_group3_Output.xml
-perl surebetCrafter.pl .\input\poland_division1_selector.xml .\results\poland_division1_Output.xml
-perl surebetCrafter.pl .\input\poland_division2_selector.xml .\results\poland_division2_Output.xml
-perl surebetCrafter.pl .\input\qatarSelector.xml .\results\qatar_Output.xml
