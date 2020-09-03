236c2e6d049317dd44052979b7010a1592ac6d6b
Wojciech Marzec <wojomc81@gmail.com>
2020-09-03 21:19:35 +0200

    [I] added class responsible for Xpath handling

---
+++ cgi-bin/BookmakerOfferDownloader/Xpath.pm
@@ -0,0 +1,33 @@
+package Xpath;
+#
+#===============================================================================
+#
+#              FILE: Xpath.pm
+#
+#  RESPONSIBILITIES: 
+#
+#             FILES: ---
+#              BUGS: ---
+#             NOTES: ---
+#            AUTHOR: WOJCIECH MARZEC (), marzec.wojciech@gmail.com
+#      ORGANIZATION: 
+#           CREATED: 09/03/2020 12:28:25 PM
+#===============================================================================
+use strict;
+use warnings;
+ 
+use Exporter;
+our @ISA = qw( Exporter );
+
+
+sub trimBeginning($$);
+
+
+sub trimBeginning($$)
+{
+	my ($xpathToDelete ,$wholeXpath ) = @_;
+	
+	$wholeXpath =~ s|^${xpathToDelete}||;
+	return $wholeXpath;
+};
+
