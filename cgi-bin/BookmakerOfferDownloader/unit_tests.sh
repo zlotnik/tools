1eb4a55e271afa4f35cbf7f3da82f5084293f8d2
Wojciech Marzec <marzec.wojciech@gmail.com>
2021-04-13 10:28:35 +0200

    [T] adding triggering HTML_EventsTableParser_test into unit_test script

--- cgi-bin/BookmakerOfferDownloader/unit_tests.sh
+++ cgi-bin/BookmakerOfferDownloader/unit_tests.sh
@@ -1,8 +1,15 @@
 source /var/www/cgi-bin/environment.sh
 perl $@ $BOOKMAKER_OFFER_DOWNLOADER_MODULE_DIRECTORY/tests/unit_tests/BetExplorerDownloader/BetExplorerDownloader_test.pl
+
 if [ $? == 0 ]
 then
 perl $@ $BOOKMAKER_OFFER_DOWNLOADER_MODULE_DIRECTORY/tests/unit_tests/BetexplorerParser/BetexplorerParser_test.pl
 fi
+
+if [ $? == 0 ]
+then
+perl $@ $BOOKMAKER_OFFER_DOWNLOADER_MODULE_DIRECTORY/tests/unit_tests/HTML_EventsTableParser/HTML_EventsTableParser_test.pl
+fi
+
 
 
