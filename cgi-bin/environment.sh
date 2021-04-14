9eb17a74c946f5ab637f6f733725f6da948dbb5b
Wojciech Marzec <marzec.wojciech@gmail.com>
2021-04-14 10:50:29 +0200

    [I] second amendment for showing diffrent command prompt after sourcing

--- cgi-bin/environment.sh
+++ cgi-bin/environment.sh
@@ -14,4 +14,4 @@ export PERL5LIB=$PERL5LIB:$BACKEND_ROOT_DIRECTORY/modules/perl5
 export PERL5LIB=$PERL5LIB:$BACKEND_ROOT_DIRECTORY/modules/lib/perl5
 export PERL5LIB=$PERL5LIB:$BOOKMAKER_OFFER_DOWNLOADER_MODULE_DIRECTORY/tests/unit_tests/BetexplorerParser
 
-export PS1='[\u@\h \W]\e[0;32;1m\$\e[m'
+export PS1='[\u@\h \W]->\$'
